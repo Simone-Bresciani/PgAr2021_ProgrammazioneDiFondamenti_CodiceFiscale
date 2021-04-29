c29f71de99dcebfbf8396135377ea67b9cd363cb
simone bresciani <s.bresciani026@studenti.unibs.it>
2021-04-29 15:29:45 +0200

    Creazione metodi per classe CodiceFiscale

--- src/it/unibs/arnaldo/codiceFiscale/CodiceFiscale.java
+++ src/it/unibs/arnaldo/codiceFiscale/CodiceFiscale.java
@@ -1,13 +1,15 @@
 package it.unibs.arnaldo.codiceFiscale;
 
+import mylib.ControlloDati;
+
 public class CodiceFiscale {
 
 
     private String cognome;
     private String nome;
-    private int anno_di_nascita;
+    private String anno_di_nascita;
     private char mese_di_nascita;
-    private int giorno_di_nascita;
+    private String giorno_di_nascita;
     private String luogo_di_nascita;
     private char controllo;
 
@@ -30,11 +32,11 @@ public class CodiceFiscale {
         this.nome = nome;
     }
 
-    public int getAnno_di_nascita() {
+    public String getAnno_di_nascita() {
         return anno_di_nascita;
     }
 
-    public void setAnno_di_nascita(int anno_di_nascita) {
+    public void setAnno_di_nascita(String anno_di_nascita) {
         this.anno_di_nascita = anno_di_nascita;
     }
 
@@ -46,11 +48,11 @@ public class CodiceFiscale {
         this.mese_di_nascita = mese_di_nascita;
     }
 
-    public int getGiorno_di_nascita() {
+    public String getGiorno_di_nascita() {
         return giorno_di_nascita;
     }
 
-    public void setGiorno_di_nascita(int giorno_di_nascita) {
+    public void setGiorno_di_nascita(String giorno_di_nascita) {
         this.giorno_di_nascita = giorno_di_nascita;
     }
 
@@ -71,7 +73,7 @@ public class CodiceFiscale {
     }
 
     //COSTRUTTORE
-    public CodiceFiscale(String cognome, String nome, int anno_di_nascita, char mese_di_nascita, int giorno_di_nascita, String luogo_di_nascita, char controllo) {
+    public CodiceFiscale(String cognome, String nome, String anno_di_nascita, char mese_di_nascita, String giorno_di_nascita, String luogo_di_nascita, char controllo) {
         this.cognome = cognome;
         this.nome = nome;
         this.anno_di_nascita = anno_di_nascita;
@@ -80,6 +82,22 @@ public class CodiceFiscale {
         this.luogo_di_nascita = luogo_di_nascita;
         this.controllo = controllo;
     }
+
+    public boolean validaCodiceFiscale(){
+        return (controllaNomeCognome() && controllaAnnoDiNascita() && controllaMeseDiNascita() &&
+                controllaGiornoDiNascita() && controllaLuogoDiNascita() && controllaControllo() );
+    }
+
+
+
+
+
+
+
+    private boolean controllaAnnoDiNascita(){
+
+    }
+
 
     //TOSTRING
     @Override
